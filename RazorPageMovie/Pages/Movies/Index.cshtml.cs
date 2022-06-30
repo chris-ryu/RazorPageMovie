a828637a5ad56d5381ecb45ab69a08d22cc9d9b9
류경석 <narzis@gmail.com>
2022-06-30 12:10:40 +0900

    초기 커밋.

---
+++ RazorPageMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+
+namespace RazorPageMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageMovie.Data.RazorPageMovieContext _context;
+
+        public IndexModel(RazorPageMovie.Data.RazorPageMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
